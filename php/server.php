7ae02a510437f06b76b1ad64cd183ff4cf66b234
kolarvasek <kolarva23@sps-prosek.cz>
2025-01-07 09:19:16 +0100

    qwe

--- php/server.php
+++ php/server.php
@@ -9,9 +9,26 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $rawData = file_get_contents("php://input");
 $data = json_decode($rawData, true);
-
 $items = $data['items'];
-echo json_encode($items);   
+
+foreach ($items as $item) {
+            $name = $item['name'];
+            $calories = $item['calories'];
+            $protein = $item['protein_g'];
+            $carbs = $item['carbohydrates_total_g'];
+            $fat = $item['fat_total_g'];
+
+            $sql = "INSERT INTO food (name, user_id, calories, protein, carbs, fat, ) VALUES (:name,  :user_id, :calories, :protein, :carbs, :fat)";
+            $stmt = $db->prepare($sql);
+            $stmt->execute([
+                ':name' => $name,
+                ':calories' => $calories,
+                ':protein' => $protein,
+                ':carbs' => $carbs,
+                ':fat' => $fat,
+                ':user_id' => $_SESSION['user_id']
+            ]);
+        }
 
 
 
